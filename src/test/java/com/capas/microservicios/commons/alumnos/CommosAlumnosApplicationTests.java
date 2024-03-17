062749cec85afcb3ba03977e06753521191acc9e
Angelo <gangelos630@gmail.com>
2024-03-16 20:49:55 -0500

    subiendo commons-alumnos

---
+++ src/test/java/com/capas/microservicios/commons/alumnos/CommosAlumnosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capas.microservicios.commons.alumnos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommosAlumnosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
